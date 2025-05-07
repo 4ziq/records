d4cf5fb69a59ce4ebefb98081b2166fcb47489e7
4ziq <96971890+4ziq@users.noreply.github.com>
2025-05-08 02:22:35 +0800

    add navbar

---
+++ frontend/app/(tabs)/createReview.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { styles } from "../../styles/styles";
+
+export default function CreateReview() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>review</Text>
+    </View>
+  );
+}
---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "../../styles/styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
